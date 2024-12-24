1982ea3fe094e6f6d39baea908a959336431551e
Shivam Ahirwar <shivamahirwar0917109@gmail.com>
2024-12-25 00:20:50 +0530

    add new code

--- DSA_Basic/2D_Array/Code1.cpp
+++ DSA_Basic/2D_Array/Code1.cpp
@@ -5,13 +5,37 @@ using namespace std;
 
 int main(){
 
-   int arr[4][4]= {{1,2,3,4},
-                   {4,5,6,7},
-                   {6,7,8,9},
-                   {6,5,4,3} };
+   int arr[4][4] = {{1,2,3,5},
+                   {4,5,6,8},
+                   {6,7,2,3},
+                   {6,3,4,5} };
     int row = 4;
     int col = 4;  
-    // row vies             
+
+// col vies
+// for(int j=0; j<col; j++){
+//     for(int i=0; i<row; i++){
+//         cout<<arr[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+
+//diagnal vies --> only have square matric 
+// for(int i=0; i<row; i++){
+//     for(int j=0; j<col; j++){
+//         if( row == col){
+//             cout<<arr[i][j]<<" ";
+//         }
+        
+//     }
+//     cout<<endl;
+// }
+//optinal methid 
+for(int i=0; i<row; i++){
+    cout<<arr[i][i] <<" ";
+}
+
+// row vies             
     //  for(int i=0; i<row; i++){
     //     for(int j=0; j<col; j++){
     //         cout<<arr[i][j]<<" ";
@@ -19,13 +43,13 @@ int main(){
     //     cout<<endl;
     //  }
 
-// col vise
-for(int i=0; i<row; i++){
-    for(int j=0; j<col; j++){
-        cout<<arr[j][i]<<" ";
-    }
-    cout<<endl;
-}
+// squer matric 
+// for(int i=0; i<row; i++){
+//     for(int j=0; j<col; j++){
+//         cout<<arr[j][i]<<" ";
+//     }
+//     cout<<endl;
+// }
 
     return 0;
 }
--- DSA_Basic/2D_Array/tempCodeRunnerFile.cpp
+++ DSA_Basic/2D_Array/tempCodeRunnerFile.cpp
@@ -1,6 +1,1 @@
-    //  for(int i=0; i<row; i++){
-    //     for(int j=0; j<col; j++){
-    //         cout<<arr[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    //  }
+ 
